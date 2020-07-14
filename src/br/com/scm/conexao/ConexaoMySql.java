1138d6e86c7b13c1f9dbc361e67e2c1e3cf29e1a
gabriel <gabrielalmeidads@gmail.com>
2020-07-13 21:24:15 -0300

    Alterand classe de conexão

--- src/br/com/scm/conexao/ConexaoMySql.java
+++ src/br/com/scm/conexao/ConexaoMySql.java
@@ -7,34 +7,55 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 /**
  * @author Gabriel
  */
 public abstract class ConexaoMySql {
     
-    
-    
-    public static Connection getConnection (){
-        String path = "src\\br\\com\\scm\\properties\\db.properties";
+    public static Connection conectar (){
         try{
-            Properties props = Funcoes.getPropertie(path);
-            String url = props.getProperty("dburl");
-            return DriverManager.getConnection(url, props); 
+            Properties props = Funcoes.getProperties("src\\br\\com\\scm\\properties\\db.properties");        
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection(props.getProperty("dburl"), props.getProperty("user"), props.getProperty("password"));            
         }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Falha ao estabelecer conexão com banco de dados, entre em contato com o suporte do sistema ");
             e.printStackTrace();
             return null;
         }
     }
     
-    public static void closeConnection(Connection con){
+    public static void desconectar(Connection con){
         if (con != null){
             try {
                 con.close();
             } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Falha ao fechar conexão com banco de dados, entre em contato com o suporte do sistema");            
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public static void desconectar(Connection con, PreparedStatement stmt){
+        desconectar(con);
+        if (stmt != null){
+            try {
+                stmt.close();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Falha ao fechar conexão com banco de dados, entre em contato com o suporte do sistema");            
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public static void desconectar(Connection con, PreparedStatement stmt, ResultSet rs){
+        desconectar(con, stmt);
+        if (rs != null){
+            try {
+                rs.close();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Falha ao fechar conexão com banco de dados, entre em contato com o suporte do sistema");            
                 e.printStackTrace();
             }
         }
