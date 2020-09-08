70f01a8833068fa06051bc7d590298f61ce093b2
Suporte T.I 2 <Suporte T.I 2@DESKTOP-TJ92AJD>
2020-09-08 15:15:55 -0300

    Form Funcionario

--- src/modelo/Funcionario.java
+++ src/modelo/Funcionario.java
@@ -12,9 +12,6 @@ package modelo;
 public class Funcionario {
     private int Cod_Funcionario;
     private int Cod_Cargo;
-    private int Cod_Usuario_Cadastro;
-    private int Cod_estado;
-    private int Cod_cidade;
     private String Nome_Funcionario;
     private String Data_Nascimento;
     private String Rg_Funcionario;
@@ -26,12 +23,9 @@ public class Funcionario {
     public Funcionario() {
     }
 
-    public Funcionario(int Cod_Funcionario, int Cod_Cargo, int Cod_Usuario_Cadastro, int Cod_estado, int Cod_cidade, String Nome_Funcionario, String Data_Nascimento, String Rg_Funcionario, String CPF_Funcionario, String WhatsApp_Funcionario, String Endereco_Funcionario, boolean ativo) {
+    public Funcionario(int Cod_Funcionario, int Cod_Cargo, String Nome_Funcionario, String Data_Nascimento, String Rg_Funcionario, String CPF_Funcionario, String WhatsApp_Funcionario, String Endereco_Funcionario, boolean ativo) {
         this.Cod_Funcionario = Cod_Funcionario;
         this.Cod_Cargo = Cod_Cargo;
-        this.Cod_Usuario_Cadastro = Cod_Usuario_Cadastro;
-        this.Cod_estado = Cod_estado;
-        this.Cod_cidade = Cod_cidade;
         this.Nome_Funcionario = Nome_Funcionario;
         this.Data_Nascimento = Data_Nascimento;
         this.Rg_Funcionario = Rg_Funcionario;
@@ -40,21 +34,13 @@ public class Funcionario {
         this.Endereco_Funcionario = Endereco_Funcionario;
         this.ativo = ativo;
     }
-    
-    
 
-    public Funcionario(int Cod_Funcionario, int Cod_Cargo, int Cod_Usuario, int Cod_estado, int Cod_cidade, String Nome_Funcionario, String Data_Nascimento, String Rg_Funcionario, String CPF_Funcionario, String WhatsApp_Funcionario, String Endereco_Funcionario) {
+    public int getCod_Funcionario() {
+        return Cod_Funcionario;
+    }
+
+    public void setCod_Funcionario(int Cod_Funcionario) {
         this.Cod_Funcionario = Cod_Funcionario;
-        this.Cod_Cargo = Cod_Cargo;
-        this.Cod_Usuario_Cadastro = Cod_Usuario;
-        this.Cod_estado = Cod_estado;
-        this.Cod_cidade = Cod_cidade;
-        this.Nome_Funcionario = Nome_Funcionario;
-        this.Data_Nascimento = Data_Nascimento;
-        this.Rg_Funcionario = Rg_Funcionario;
-        this.CPF_Funcionario = CPF_Funcionario;
-        this.WhatsApp_Funcionario = WhatsApp_Funcionario;
-        this.Endereco_Funcionario = Endereco_Funcionario;
     }
 
     public int getCod_Cargo() {
@@ -63,40 +49,6 @@ public class Funcionario {
 
     public void setCod_Cargo(int Cod_Cargo) {
         this.Cod_Cargo = Cod_Cargo;
-    }
-
-    public int getCod_Usuario_Cadastro() {
-        return Cod_Usuario_Cadastro;
-    }
-
-    public void setCod_Usuario_Cadastro(int Cod_Usuario_Cadastro) {
-        this.Cod_Usuario_Cadastro = Cod_Usuario_Cadastro;
-    }
-
-    public int getCod_estado() {
-        return Cod_estado;
-    }
-
-    public void setCod_estado(int Cod_estado) {
-        this.Cod_estado = Cod_estado;
-    }
-
-    public int getCod_cidade() {
-        return Cod_cidade;
-    }
-
-    public void setCod_cidade(int Cod_cidade) {
-        this.Cod_cidade = Cod_cidade;
-    }
-
-    
-
-    public int getCod_Funcionario() {
-        return Cod_Funcionario;
-    }
-
-    public void setCod_Funcionario(int Cod_Funcionario) {
-        this.Cod_Funcionario = Cod_Funcionario;
     }
 
     public String getNome_Funcionario() {
@@ -154,7 +106,5 @@ public class Funcionario {
     public void setAtivo(boolean ativo) {
         this.ativo = ativo;
     }
-    
-    
-    
+
 }
